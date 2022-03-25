f7717fc3d2a98ee6361b3022930a98f1c7831f24
Ravi Patel <40341676+ravilp92@users.noreply.github.com>
2022-03-25 16:27:40 -0500

    Added Vue Base Project

---
+++ ecom/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row mt-5">
+    <b-card-group deck>
+      <b-card
+        header="featured"
+        header-tag="header"
+        footer="Card Footer"
+        footer-tag="footer"
+        title="Title"
+      >
+        <b-card-text>Header and footers using props.</b-card-text>
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
+    </b-card-group>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+  components: {},
+});
+</script>
